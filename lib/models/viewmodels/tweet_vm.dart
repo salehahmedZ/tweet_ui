5131f7a0c8f5ebd79b495fa80ed61ca6a0d060de
Saleh Alzahrani <saleh_ahmed@outlook.com>
2022-05-17 04:48:41 +0300

    Update tweet_vm.dart

--- lib/models/viewmodels/tweet_vm.dart
+++ lib/models/viewmodels/tweet_vm.dart
@@ -261,8 +261,7 @@ class TweetVM {
     final List<Variant>? listOfVideoVariants = _videoEntity(tweet)?.videoInfo?.variants.where((variant) => variant.contentType == 'video/mp4').toList();
     listOfVideoVariants?.sort((variantA, variantB) => variantA.bitrate.compareTo(variantB.bitrate));
     if (listOfVideoVariants != null && listOfVideoVariants.isNotEmpty) {
-      return Map.fromIterable(listOfVideoVariants,
-          key: (dynamic variant) => (variant as Variant).bitrate.toString() + ' kbps', value: (dynamic variant) => (variant as Variant).url);
+      return Map.fromIterable(listOfVideoVariants, key: (dynamic variant) => (variant as Variant).bitrate.toString() + ' kbps', value: (dynamic variant) => (variant as Variant).url);
     } else {
       return {};
     }
